b91fd0bbefaa33563e895cb7e23641f6f580358d
peternguyen39 <54886011+peternguyen39@users.noreply.github.com>
2021-12-25 16:54:14 +0700

    Add navigation, app bar init

--- app/src/main/java/com/example/time2meet/ui/FragmentEditProfile.java
+++ app/src/main/java/com/example/time2meet/ui/FragmentEditProfile.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.time2meet.R;
 
@@ -16,45 +18,17 @@ import com.example.time2meet.R;
  * create an instance of this fragment.
  */
 public class FragmentEditProfile extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    
 
     public FragmentEditProfile() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentEditProfile.
-     */
+    
     // TODO: Rename and change types and number of parameters
-    public static FragmentEditProfile newInstance(String param1, String param2) {
-        FragmentEditProfile fragment = new FragmentEditProfile();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -62,5 +36,22 @@ public class FragmentEditProfile extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_edit_profile, container, false);
+    }
+    private void initAppBar(){
+        TextView appbar_title = (TextView) getView().findViewById(R.id.tv_action_bar_center);
+        appbar_title.setText(getResources().getString(R.string.my_profile));
+        ImageButton back_button = (ImageButton) getView().findViewById(R.id.btn_action_bar_leftmost);
+        ImageButton save_button =(ImageButton) getView().findViewById(R.id.btn_action_bar_rightmost);
+        back_button.setImageResource(R.drawable.ic_back);
+        save_button.setImageResource(R.drawable.ic_save);
+        save_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setEdited(view);
+            }
+        });
+    }
+
+    private void setEdited(View view) {
     }
 }
--- app/src/main/java/com/example/time2meet/ui/FragmentProfile.java
+++ app/src/main/java/com/example/time2meet/ui/FragmentProfile.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.time2meet.R;
 
@@ -26,5 +28,23 @@ public class FragmentProfile extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+    private void initAppBar(){
+        TextView appbar_title = (TextView) getView().findViewById(R.id.tv_action_bar_center);
+        appbar_title.setText(getResources().getString(R.string.my_profile));
+        ImageButton back_button = (ImageButton) getView().findViewById(R.id.btn_action_bar_leftmost);
+        ImageButton edit_button =(ImageButton) getView().findViewById(R.id.btn_action_bar_rightmost);
+        back_button.setImageResource(R.drawable.ic_back);
+        edit_button.setImageResource(R.drawable.ic_edit);
+        edit_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setEdited(view);
+            }
+        });
+    }
+
+    private void setEdited(View view) {
+
     }
 }
