a7353c2fe8422ab42b442e34adb96e82888bbd30
Nguyễn Ngọc Thiện <nnthien2001@gmail.com>
2021-12-30 22:06:52 +0700

    fragment EditMeeting + fragment ViewMeeting partly done
    rename fragment ViewMeeting
    add observer in fragment ViewMeeting
    basic functionality in fragment EditMeeting
    build ok, did not run :trollface:

--- app/src/main/java/com/example/time2meet/data/MeetingViewModel.java
+++ app/src/main/java/com/example/time2meet/data/MeetingViewModel.java
@@ -1,5 +1,6 @@
 package com.example.time2meet.data;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
 import java.util.ArrayList;
@@ -19,8 +20,16 @@ public class MeetingViewModel extends ViewModel {
         return meetingRepository.getMeeting().getValue();
     }
 
+    public LiveData<Meeting> getMeetingLiveDate() {
+        return meetingRepository.getMeeting();
+    }
+
     public Integer goMeeting(Integer meetingID) {
         return meetingRepository.goMeeting(meetingID);
+    }
+
+    public Integer updateMeeting(String meetingName, String date, String location, String description) {
+        return meetingRepository.updateMeeting(meetingName, date, location, description);
     }
 
     public  Integer toggleTime(ArrayList<Integer>interval){
