62030bab5b167b997f0beec06dee5ded24093325
Nguyễn Ngọc Thiện <nnthien2001@gmail.com>
2021-12-30 20:00:04 +0700

    fragment MeetingMenu (aka ViewMeeting)
    add view binding and data binding
    basic layout of fragment
    did not set up flow into this fragment, have to wait for pop-up in Home
    build ok, did not run :trollface:

--- app/src/main/java/com/example/time2meet/ui/FragmentMeetingMenu.java
+++ app/src/main/java/com/example/time2meet/ui/FragmentMeetingMenu.java
@@ -18,13 +18,15 @@ import android.widget.EditText;
 import com.example.time2meet.R;
 import com.example.time2meet.data.Meeting;
 import com.example.time2meet.data.MeetingViewModel;
+import com.example.time2meet.databinding.FragmentMeetingMenuBinding;
 
-public class FragmentMeetingMenu extends Fragment implements View.OnClickListener {
+public class FragmentMeetingMenu extends Fragment {
 
+    private FragmentMeetingMenuBinding binding;
     private NavController navController;
     private MeetingViewModel meetingViewModel;
-
-    private EditText meeting_tv;
+    private Meeting meeting;
+    private Integer meetingID;
 
     public FragmentMeetingMenu() {
         // Required empty public constructor
@@ -33,13 +35,18 @@ public class FragmentMeetingMenu extends Fragment implements View.OnClickListene
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            meetingID = getArguments().getInt("meetingID");
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_meeting_menu, container, false);
+        binding = FragmentMeetingMenuBinding.inflate(inflater, container, false);
+
+        //return inflater.inflate(R.layout.fragment_meeting_menu, container, false);
+        return binding.getRoot();
     }
 
     @Override
@@ -50,22 +57,38 @@ public class FragmentMeetingMenu extends Fragment implements View.OnClickListene
         NavBackStackEntry backStackEntry = navController.getBackStackEntry(R.id.meeting_nav_graph);
         meetingViewModel = new ViewModelProvider(backStackEntry).get(MeetingViewModel.class);
 
-        meeting_tv = (EditText) view.findViewById(R.id.meeting_edittext);
-        view.findViewById(R.id.edit_meeting_btn).setOnClickListener(this);
-        view.findViewById(R.id.st_edit_meeting_btn).setOnClickListener(this);
+        // set up meeting
+        meetingViewModel.goMeeting(meetingID);
+        meeting = meetingViewModel.getMeeting();
+
+        // binding view
+        binding.meetingStartDate.setText(meeting.getStartDate());
+        binding.meetingEndDate.setText(meeting.getEndDate());
+        if (meeting.getDate() != null) {
+            binding.meetingProposedTime.setText(meeting.getDate());
+        }
+        binding.meetingLocation.setText(meeting.getLocation());
+        binding.meetingHost.setText(meetingViewModel.getHost().getName());
+        binding.meetingMeetingId.setText(meeting.getMeetingID());
+        binding.meetingDescription.setText(meeting.getDescription());
+
+        binding.meetingBtnViewAttendees.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.action_fragmentMeetingMenu_to_fragmentViewAttendee);
+            }
+        });
+        binding.meetingBtnViewAvailability.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.action_fragmentMeetingMenu_to_fragmentAvailabilityBoard);
+            }
+        }));
     }
 
     @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.edit_meeting_btn) {
-//            Meeting meeting = new Meeting(meeting_tv.getText().toString());
-//
-//            meetingViewModel.setMeeting(meeting);
-//            navController.navigate(R.id.action_fragmentMeetingMenu_to_fragmentEditMeeting);
-        }
-
-        if (v.getId() == R.id.st_edit_meeting_btn) {
-            navController.navigate(R.id.action_fragmentMeetingMenu_to_fragmentAvailabilityBoard);
-        }
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
