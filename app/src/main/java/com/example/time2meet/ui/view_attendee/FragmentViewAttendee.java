1ff7836c7639c5e2b9e7cb8032c5f168206b93aa
nguyenthieukhang <49133713+nguyenthieukhang@users.noreply.github.com>
2022-01-03 10:15:37 +0700

    Remove the click to Profile

--- app/src/main/java/com/example/time2meet/ui/view_attendee/FragmentViewAttendee.java
+++ app/src/main/java/com/example/time2meet/ui/view_attendee/FragmentViewAttendee.java
@@ -122,31 +122,31 @@ public class FragmentViewAttendee extends Fragment implements View.OnClickListen
         mSectionedAdapter.setSections();
 
         // View user profile operation
-        recyclerView.addOnItemTouchListener(
-                new RecyclerItemClickListener(getContext(), recyclerView ,new RecyclerItemClickListener.OnItemClickListener() {
-
-                    @Override
-                    public void onItemClick(View view, int position) {
-                    }
-
-                    @Override public void onLongItemClick(View view, int position) {
-                        if(position==0 || position==2) {
-                        }
-                        else if(position==1) {
-                            goToUserProfile(host.getUserID());
-                        }
-                        else {
-                            goToUserProfile(adapter.getUsers().get(position-2).getUserID());
-                        }
-                    }
-
-                    private void goToUserProfile(Integer userID) {
-                        Bundle bundle = new Bundle();
-                        bundle.putInt("userID", userID);
-                        navController.navigate(R.id.action_fragmentViewAttendee_to_fragmentProfile, bundle);
-                    }
-                })
-        );
+//        recyclerView.addOnItemTouchListener(
+//                new RecyclerItemClickListener(getContext(), recyclerView ,new RecyclerItemClickListener.OnItemClickListener() {
+//
+//                    @Override
+//                    public void onItemClick(View view, int position) {
+//                    }
+//
+//                    @Override public void onLongItemClick(View view, int position) {
+//                        if(position==0 || position==2) {
+//                        }
+//                        else if(position==1) {
+//                            goToUserProfile(host.getUserID());
+//                        }
+//                        else {
+//                            goToUserProfile(adapter.getUsers().get(position-2).getUserID());
+//                        }
+//                    }
+//
+//                    private void goToUserProfile(Integer userID) {
+//                        Bundle bundle = new Bundle();
+//                        bundle.putInt("userID", userID);
+//                        navController.navigate(R.id.action_fragmentViewAttendee_to_fragmentProfile, bundle);
+//                    }
+//                })
+//        );
 
         if(isHostOfThisMeeting()) {
             new ItemTouchHelper(itemTouchHelperCallBack).attachToRecyclerView(recyclerView);
